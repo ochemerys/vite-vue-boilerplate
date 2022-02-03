af6173c19f78aa0884da04ee47752809125bb47a
oleks <ochemerys@visionthechconsulting.ca>
2022-02-02 21:18:50 -0400

    setup cypres tests unit and e2e

---
+++ src/components/count-button.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+import { useStore, Mutation } from "../store";
+
+const store = useStore();
+const count = computed(() => store.state.count);
+
+const props = defineProps<{
+  text: string
+}>()
+
+function increment() {
+  store.commit(Mutation.INCREMENT, 1);
+}
+</script>
+
+<template>
+  <button
+    @click="increment"
+  >
+    {{props.text}}: {{ count }}
+  </button>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import CountButton from '../components/count-button.vue'
 
-import { useStore, Mutation } from "../store";
-
-const store = useStore();
-const count = computed(() => store.state.count);
-
-function increment() {
-  store.commit(Mutation.INCREMENT, 1);
-}
 </script>
 
 <template>
   <h1>Home</h1>
-  <button
-    @click="increment"
-  >
-    count is: {{ count }}
-  </button>
+  <count-button text="COUNT" />
 </template>
